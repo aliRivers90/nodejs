14b353aa92f9d878123e95fa2227c13cde3b0e7c
Ali_Rivers <ali.rivers90@gmail.com>
2025-03-18 23:56:46 +0330

    nodejs

---
+++ calculator.js
@@ -0,0 +1,28 @@
+(function(exports,require,module,__fillname,__dirname){
+  
+  function additation(a,b){
+    return a + b
+  }
+  function subtraction(a,b){
+    return a - b
+  }
+  function multiplication(a,b){
+    return a * b
+  }
+    function division(a,b){
+      return a / b
+    }
+
+})
+
+console.log("filename",__filename)
+console.log("dirname",__dirname)
+
+// module.exports.additation = additation 
+// module.exports.subtraction = subtraction
+// module.exports.multiplication = multiplication
+// module.exports.division = division
+// module.exports = {
+//   additation : additation,
+//   division : division
+// }
